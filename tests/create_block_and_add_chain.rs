90f0c8b88dc7fefd3171629e88a3a6584a546a7b
guhkadoo <gustavokadoo@hotmail.com>
2025-03-19 11:29:13 -0300

    integration test for create a block and add it to the chain

---
+++ tests/create_block_and_add_chain.rs
@@ -0,0 +1,23 @@
+use CleytoCoin::chain::{Chain, wallet::Wallet, block::Block, transaction::{Transaction, TransactionInfo}};
+use chrono::Utc;
+
+#[test]
+fn create_block_and_add_chain() {
+    let (wallet1, mut wallet1_pk) = Wallet::new();
+    let (wallet2, _) = Wallet::new();
+
+    let transaction_info = TransactionInfo::new(10.5, Utc::now());
+
+    let signature = match wallet1_pk.sign_transaction(&transaction_info) {
+        Ok(value) => value,
+        Err(e) => panic!("Error creating signed message: {e}"),
+    };
+
+    let new_transaction = Transaction::new(wallet1, wallet2, transaction_info, signature);
+
+    let mut chain = Chain::new();
+
+    let block = Block::new(&mut chain, vec![new_transaction]);
+
+    chain.add_block(block);
+}
