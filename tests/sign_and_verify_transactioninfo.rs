1955bc0e945404627225877371cb9f46834e3478
dantee-e <ramacciotti.dante@gmail.com>
2025-04-08 20:41:50 +0200

    added test to check serialization of transactions

--- tests/sign_and_verify_transactioninfo.rs
+++ tests/sign_and_verify_transactioninfo.rs
@@ -1,4 +1,4 @@
-use cleyto_coin::chain::{transaction::TransactionInfo, wallet::Wallet};
+use cleyto_coin::chain::{transaction::{Transaction, TransactionInfo}, wallet::Wallet};
 use chrono::Utc;
 
 #[test]
@@ -17,4 +17,26 @@ fn sign_and_verify_transactioninfo() {
     } else {
         println!("transaction not verified");
     }
+}
+
+#[test]
+fn serialize_and_deserialize_transaction() {
+    let (wallet, mut wallet_pk) = Wallet::new();
+    let (mallet, _) = Wallet::new();
+    let transactioninfo: TransactionInfo = TransactionInfo::new(12345 as f32, Utc::now());
+
+    let signature = match wallet_pk.sign_transaction(&transactioninfo) {
+        Ok(signed_hashed_message) => signed_hashed_message,
+        _ => panic!("error while signing transaction"),
+    };
+
+    let transaction = Transaction::new(wallet, mallet, transactioninfo, signature).unwrap();
+
+    let serialized_transaction = transaction.serialize();
+    println!("serialized_transaction: \n{serialized_transaction}");
+
+    let deserialized_transaction = match Transaction::deserialize(serialized_transaction) {
+        Ok(value) => {println!("Success deserializing transaction"); value},
+        Err(_) => panic!("Error deserializing transaction"),
+    };
 }
